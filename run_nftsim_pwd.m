c5f01ed98c4a7b7130347930e9b8044a9b7b2fd0
danpolus <danpol@gmail.com>
2022-03-19 12:11:50 +0200

    use nftsim from same folder

--- run_nftsim_pwd.m
+++ run_nftsim_pwd.m
@@ -1,27 +1,33 @@
 % runs NFTsim from every directory
-function nf_struct = run_nftsim_pwd(configuration, nftsim_path)
+% inputs:
+%   configuration - file name
+%   nftsim_path -
+%   cd_flg - move to nft folder or stay in the current one
+% outputs:
+%   nf_struct
+%
+function nf_struct = run_nftsim_pwd(configuration, nftsim_path, cd_flg)
 
-in_fn = ['.\configs\' configuration '.conf'];
-out_fn = ['.\output\' configuration '.output'];
-addpath(nftsim_path);
+if cd_flg
+    fp = '.';
+    addpath(nftsim_path);
+    work_dir = pwd;
+    cd(nftsim_path);
+else
+    fp = nftsim_path;
+end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%
-
-work_dir = pwd;
-cd(nftsim_path);
+in_fn = [fp '\configs\' configuration '.conf'];
+out_fn = [fp '\output\' configuration '.output'];
 disp(['NFTsim data generation: ' configuration]);
-[status, cmdout] = system(['.\bin\nftsim.exe -i ' in_fn ' -o ' out_fn]);
-if status ~= 0
+[status, cmdout] = system([fp '\bin\nftsim.exe -i ' in_fn ' -o ' out_fn]);
+if status == 0
+    nf_struct = nf.read(out_fn);
+    nf_struct.conf_file = configuration;
+else
     error(cmdout);
 end
-% if exist(out_fn,'file') ~=2
-%     % nf_struct = nf.run(in_fn, false, [nftsim_path '\bin\nftsim.exe']);
-%     [status, cmdout] = system(['.\bin\nftsim.exe -i ' in_fn ' -o ' out_fn]);
-%     if status ~= 0
-%         delete(out_fn);
-%         error(cmdout);
-%     end
-% end
-nf_struct = nf.read(out_fn);
-nf_struct.conf_file = configuration;
-cd(work_dir);
+
+if cd_flg
+    cd(work_dir);
+end
